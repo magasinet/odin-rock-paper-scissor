13fc06fdcd881f738e60fc1e90aa02de4c2039d6
Ali Sherifi <ali.sherifi@live.com>
2022-07-25 20:00:56 +0200

    Create rest of the functions

--- index.js
+++ index.js
@@ -32,8 +32,80 @@ Restart
 
 */
 
+// Computer Value
 function getComputerChoice() {
     const choices = ["ROCK", "PAPER", "SCISSOR"];
-    return choices[Math.floor(Math.random()*choices.length)];
+    return choices[Math.floor(Math.random() * choices.length)];
 
 }
+
+// player variable to be added with points after each game.
+let playerPoints = 0;
+let computerPoints = 0;
+
+//Compare Values
+function playRound(playerSelection, computerSelection) {
+   
+    // make player input not case-sensitive
+    let playerHand = playerSelection.toUpperCase();
+
+
+    //compare hands and add 1 point to the winner
+    if (playerHand === "ROCK" && computerSelection === "PAPER") {
+        computerPoints += 1;
+        console.log("You lose! Paper beats Rock");
+
+    } else if (playerHand === "ROCK" && computerSelection === "SCISSOR") {
+        playerPoints += 1;
+        console.log("You win! Rock beats scissor");
+
+    } else if (playerHand === "ROCK" && computerSelection === "ROCK") {
+        console.log("Draw! Nobody wins");
+
+    } else if (playerHand === "PAPER" && computerSelection === "ROCK") {
+        playerPoints += 1;
+        console.log("You win! Paper beats rock");
+
+    } else if (playerHand === "PAPER" && computerSelection === "SCISSOR") {
+        computerPoints += 1;
+        console.log("You lose! Scissor beats paper");
+
+    } else if (playerHand === "PAPER" && computerSelection === "PAPER") {
+        console.log("Draw! Nobody wins");
+
+    } else if (playerHand === "SCISSOR" && computerSelection === "PAPER") {
+        playerPoints += 1;
+        console.log("You win! Scissor beats paper");
+
+    } else if (playerHand === "SCISSOR" && computerSelection === "ROCK") {
+        computerPoints += 1;
+        console.log("You lose! Rock beats scissor");
+
+    } else if (playerHand === "SCISSOR" && computerSelection === "SCISSOR") {
+        console.log("Draw! Nobody wins");
+    }
+
+}
+
+
+//run playRound/game 5 times and check who has the most score
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = prompt("Rock, paper or scissor?");
+        const computerSelection = getComputerChoice();
+        playRound(playerSelection, computerSelection);
+    }
+
+    //COMPARE POINTS AND CONSOLE FINAL RESULT
+    if (playerPoints > computerPoints) {
+        console.log(`Congratulations! You got ${playerPoints} point and the computer got ${computerPoints}`);
+    } else if (computerPoints > playerPoints) {
+        console.log(`Sorry mate! You got ${playerPoints} point and the computer got ${computerPoints}`)
+    } else {
+        console.log(`It's a draw! You got ${playerPoints} point and the computer got ${computerPoints}`);
+    }
+
+}
+
+game();
